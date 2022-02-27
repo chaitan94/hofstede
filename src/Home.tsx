3e092f6cc3995b55b05cce70cb1722b0644be352
Krishna Chaitanya <bkchaitan94@gmail.com>
2022-02-27 16:55:21 +0000

    Improve Homepage UI; Introduce gitpod

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,41 @@
-import { Card } from "antd";
+import { Avatar, List } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
+import map from './images/map.png';
+import compass from './images/compass.png';
+import { ArrowRightOutlined } from "@ant-design/icons";
 
 const Home: React.FC = () => {
+  const data = [
+    { icon: compass, title: "Explore Mode", description: "Show top 9 countries matching your criteria", link: "/explore" },
+    { icon: map, title: "Map Mode", description: "Show all countries on a 2D map, culturally distanced", link: "/map" },
+  ]
   return (
     <div
       style={{
         padding: "10vw",
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
         gap: 32,
       }}
     >
-      <Link to="/explore">
-        <Card title="Explore mode">
-          Show top 9 countries matching your criteria
-        </Card>
-      </Link>
-      <Link to="/map">
-        <Card title="Map mode">
-          Show all countries on a 2D map, culturally distanced
-        </Card>
-      </Link>
+      <h1>Hofstede</h1>
+      <p>Select one of the below to continue.</p>
+      <List
+        itemLayout="horizontal"
+        size="large"
+        dataSource={data}
+        renderItem={item => (
+          <Link to={item.link}>
+            <List.Item style={{ background: 'white' }} actions={[<ArrowRightOutlined />]}>
+              <List.Item.Meta
+                avatar={<Avatar shape="square" size="large"  src={item.icon} />}
+                title={item.title}
+                description={item.description}
+              />
+            </List.Item>
+          </Link>
+        )}
+      />
     </div>
   );
 };
